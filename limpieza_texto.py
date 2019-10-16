324580dd7883e0451b46e0d404861bcf3029a62b
baldesco <eduardoescobar7@gmail.com>
2019-10-16 16:10:35 -0500

    actualizar listas de nombres y municipios/departamentos de Colombia

--- limpieza_texto.py
+++ limpieza_texto.py
@@ -2,6 +2,7 @@ import itertools
 import re
 import unicodedata
 import pkg_resources
+from .lenguajes import dict_lenguajes
 
 ####### Definición de funciones para limpiar el texto  #########
 
@@ -17,11 +18,11 @@ def remover_acentos(texto):
     return str(texto)
 
 # Quita ciertas palabras y expresiones previamente indicadas
-def remover_stopwords(texto, lista_palabras=[], lista_expresiones=[], ubicacion_archivo=None, separador_espacio='__'):
+def remover_stopwords(texto, lista_palabras=[], lista_expresiones=[], ubicacion_archivo=None):
     # Si se pasa como argumento la ubicación de un archivo plano que contiene la lista de palabras y expresiones
     # no deseadas separadas por comas, espacios o por enters.
     if ubicacion_archivo:
-        lista_palabras, lista_expresiones = cargar_stopwords(ubicacion_archivo,separador_espacio)
+        lista_palabras, lista_expresiones = cargar_stopwords(ubicacion_archivo)
     # Quitar las expresiones no deseadas
     for expresion in set(lista_expresiones):
         texto = texto.replace(expresion,' ')
@@ -54,10 +55,10 @@ def limpieza_basica(texto, quitar_numeros=True):
     return texto
 
 # Limpieza básica + remover palabras de menos de n caracteres y stopwords
-def limpieza_texto(texto, lista_palabras=[], lista_expresiones=[], ubicacion_archivo=None, separador_espacio='__', n_min=0, quitar_numeros=True, quitar_acentos=False):
+def limpieza_texto(texto, lista_palabras=[], lista_expresiones=[], ubicacion_archivo=None, n_min=0, quitar_numeros=True, quitar_acentos=False):
     # Quitar palabras y expresiones no deseadas. Se hace al texto original porque la palabra/expresión
-    # a remover puede tener mayúsculas/signos o estar compuesta por palabras cortas
-    texto = remover_stopwords(texto,lista_palabras,lista_expresiones,ubicacion_archivo,separador_espacio)
+    # a remover puede tener tildes/mayúsculas/signos o estar compuesta por palabras cortas
+    texto = remover_stopwords(texto,lista_palabras,lista_expresiones,ubicacion_archivo)
     # Se verifica si se desean quitar acentos/tildes
     if quitar_acentos:
         texto = remover_acentos(texto)
@@ -67,7 +68,7 @@ def limpieza_texto(texto, lista_palabras=[], lista_expresiones=[], ubicacion_arc
     texto = remover_palabras_cortas(texto, n_min)
     # Se hace esto de nuevo, por si habían palabras que después de su limpieza quedan en 
     # la lista de palabras/expresiones no deseadas
-    texto = remover_stopwords(texto,lista_palabras,lista_expresiones,ubicacion_archivo,separador_espacio)    
+    texto = remover_stopwords(texto,lista_palabras,lista_expresiones,ubicacion_archivo)    
     return texto
 
 # Función para quitar frases o palabras repetidas separadas por un caracter
@@ -77,20 +78,24 @@ def quitar_repetidos(texto, sep='|'):
     lista = set(lista)
     return ' '.join(lista)
 
+# Función para quitar el espacio al inicio y al final de un string
+def limpiar_extremos(texto):
+    return texto[::-1].rstrip()[::-1].rstrip()
+
 # Función para obtener las listas de palabras y expresiones que se desean
 # eliminar de un texto, a partir de un archivo plano
-def cargar_stopwords(file_path, separador_espacio='__'):
+def cargar_stopwords(file_path):
     lista_palabras = []
     lista_expresiones = []
-    with open(file_path) as fp:
+    with open(file_path, encoding='utf8') as fp:
         line = fp.readline()
         while line:
             linea = line.strip().split(',')
-            linea = [i.split(' ') for i in linea]
-            linea = list(itertools.chain.from_iterable(linea))
+            # linea = [i.split(' ') for i in linea]
+            # linea = list(itertools.chain.from_iterable(linea))
             for i in linea:
-                if separador_espacio in i:
-                    i = re.sub(separador_espacio,' ', i)
+                i = limpiar_extremos(i)
+                if len(i.split(' ')) > 1:
                     lista_expresiones.append(i)
                 else:
                     lista_palabras.append(i)
@@ -100,29 +105,34 @@ def cargar_stopwords(file_path, separador_espacio='__'):
 
 # Función para cargar lista general predefinida de stopwords
 def lista_stopwords(lenguaje='es'):
-    if lenguaje == 'es':
-        ruta = pkg_resources.resource_filename(__name__, 'data/listas_stopwords/sw_es.txt')
-        sw = cargar_stopwords(ruta)[0]
-    elif lenguaje == 'en':
-        from nltk.corpus import stopwords
-        sw = stopwords.words('english')
+    lenguaje = dict_lenguajes[remover_acentos(lenguaje)]
+    if lenguaje == 'spanish':
+        try:
+            ruta = pkg_resources.resource_filename(__name__, 'data/listas_stopwords/sw_es.txt')
+            sw = cargar_stopwords(ruta)[0]
+        except:
+            from nltk.corpus import stopwords
+            sw = stopwords.words(lenguaje)
+        else:
+            from nltk.corpus import stopwords
+            sw = stopwords.words(lenguaje)
     return sw
 
 # Función para cargar lista general de stopwords
 def lista_nombres(tipo='todos'):
     if tipo == 'todos':
         ruta = pkg_resources.resource_filename(__name__, 'data/listas_stopwords/nombres.txt')
-    elif lower(tipo) in ['m','masculino','hombres']:
-        ruta = pkg_resources.resource_filename(__name__, 'data/listas_stopwords/nombres_masculinos.txt')
-    elif lower(tipo) in ['f','femenino','mujeres']:
-        ruta = pkg_resources.resource_filename(__name__, 'data/listas_stopwords/nombres_femeninos.txt')
+    elif tipo.lower() in ['m','masculino','hombre','hombres']:
+        ruta = pkg_resources.resource_filename(__name__, 'data/listas_stopwords/nombres_hombres.txt')
+    elif tipo.lower() in ['f','femenino','mujer','mujeres']:
+        ruta = pkg_resources.resource_filename(__name__, 'data/listas_stopwords/nombres_mujeres.txt')
     else:
-        return None
-    return cargar_stopwords(ruta)[0]
+        return [], []
+    return cargar_stopwords(ruta)
 
 def lista_apellidos():
     ruta =  pkg_resources.resource_filename(__name__, 'data/listas_stopwords/apellidos.txt')
-    return cargar_stopwords(ruta)[0]
+    return cargar_stopwords(ruta)
 
 def lista_geo_colombia(tipo='todos'):
     ruta_mun =  pkg_resources.resource_filename(__name__, 'data/listas_stopwords/municipios_col.txt')
@@ -131,9 +141,9 @@ def lista_geo_colombia(tipo='todos'):
         palabras = list(set(cargar_stopwords(ruta_mun)[0] + cargar_stopwords(ruta_dep)[0]))
         expresiones = list(set(cargar_stopwords(ruta_mun)[1] + cargar_stopwords(ruta_dep)[1]))
         return palabras, expresiones
-    elif lower(tipo) in ['municipios', 'mun', 'm']:
+    elif tipo.lower() in ['municipios', 'mun', 'm']:
         return cargar_stopwords(ruta_mun)
-    elif lower(tipo) in ['departamentos', 'dep', 'd']:
+    elif tipo.lower() in ['departamentos', 'dep', 'd']:
         return cargar_stopwords(ruta_dep)
     else:
-        return None
+        return [], []
