e2a805fe9b5bdcabf506592cb445a97f3588f0c7
Eduardo Escobar Gutierrez <edescobar@dnp.gov.co>
2020-08-14 10:30:48 -0500

    avances en módulo de comparación

--- contexto/comparacion_textos.py
+++ contexto/comparacion_textos.py
@@ -22,10 +22,6 @@ def jaccard_textos(texto1, texto2):
     union = set(texto1).union(set(texto2))
     return np.array([[len(intersection)/len(union)]])
 
-def jaccard_vectores(vector1, vector2):
-    return 1 - pairwise_distances(vector1, vector2, metric='jaccard')
-
-
 class Similitud():
     def __init__(self, lenguaje='es', vectorizador=None):
         """
@@ -56,85 +52,92 @@ class Similitud():
         else:
             self.vectorizador = vectorizador
 
-    def similitud_coseno(textos, denso=True):
+    def similitud_coseno(self, textos):
         if isinstance(textos, str) or len(textos) < 2:
             print ('Debe ingresar una lista de por lo menos dos textos para hacer la comparación.')
             return None
-        vectores = self.vectorizador.vectorizar(textos)
-        if len(vectores) == 2:
-            return cosine_similarity(vectores[None,0], vectores[None,1], dense_output=denso)
+        # Si se ingresan textos, estos se pasan por el vectorizador
+        if isinstance(textos[0], str):
+            textos = self.vectorizador.vectorizar(textos)
+        if len(textos) == 2:
+            return cosine_similarity(textos[None,0], textos[None,1])
         else:
-            return cosine_similarity(vectores, dense_output=denso)
+            return cosine_similarity(textos)
 
-    def similitud_jaccard(textos, vectorizar=False):
+    def similitud_jaccard(self, textos, vectorizar=False):
         n_textos = len(textos)
         if isinstance(textos, str) or n_textos < 2:
             print ('Debe ingresar una lista de por lo menos dos textos para hacer la comparación.')
             return None
         if n_textos == 2:
-            return jaccard_textos(textos[0], textos[1])
+            if isinstance(textos[0], str):
+                return jaccard_textos(textos[0], textos[1])
+            else:
+                textos = self.vectorizador.vectorizar(textos)
+                return 1 - pairwise_distances(textos[None,0], textos[None,1], metric='jaccard')
         else:
             similitudes = np.zeros((n_textos, n_textos))
-            if vectorizar:
-                vectores = self.vectorizador.vectorizar(textos)
-                for i in range(n_textos):
-                    for j in range(n_textos):
-                        similitudes[i, j] = jaccard_vectores(vectores[None,i], vectores[None,j])
+            if isinstance(textos[0], str):
+                if vectorizar:
+                    vectores = self.vectorizador.vectorizar(textos)
+                    similitudes = 1 - pairwise_distances(vectores, metric='jaccard')
+                else:
+                    for i in range(n_textos):
+                        for j in range(n_textos):
+                            similitudes[i, j] = jaccard_textos(textos[i], textos[j])
             else:
-                for i in range(n_textos):
-                    for j in range(n_textos):
-                        similitudes[i, j] = jaccard_textos(textos[i], textos[j])
+                similitudes = 1 - pairwise_distances(textos, metric='jaccard')
         return similitudes
         
+class Distancia():
+    def __init__(self, lenguaje='es', vectorizador=None):
+        """
 
+        :param lenguaje:
+        :param modelo:
+        :return:
+        """
+        # Definir lenguaje del vectorizador y vectorizador a utilizar
+        self.establecer_lenguaje(lenguaje)
+        self.establecer_vectorizador(vectorizador)
+        
+    def establecer_lenguaje(self, lenguaje):
+        """
 
-def jaccard_vectores(vector1, vector2):
+        :param lenguaje:
+        :return:
+        """
+        self.lenguaje = definir_lenguaje(lenguaje)
 
+    def establecer_vectorizador(self, vectorizador):        
+        # Definir modelo para vectorizar
+        if vectorizador is None:
+            # vectorizador por defecto
+            self.vectorizador = VectorizadorWord2Vec(self.lenguaje)
+        elif isinstance(vectorizador, str):
+            self.vectorizador = cargar_objeto(vectorizador)
+        else:
+            self.vectorizador = vectorizador
 
-jaccard_similarity('hola este es un texto de prueba', 'es bueno hacer otra prueba')
+    def distancia_pares(self, textos, tipo_distancia):
+        if isinstance(textos, str) or len(textos) < 2:
+                    print ('Debe ingresar una lista de por lo menos dos textos para hacer la comparación.')
+                    return None
+        # Si se ingresan textos, estos se pasan por el vectorizador
+        if isinstance(textos[0], str):
+            textos = self.vectorizador.vectorizar(textos)
+        if len(textos) == 2:
+            return pairwise_distances(textos[None,0], textos[None,1], metric=tipo_distancia)
+        else:
+            return pairwise_distances(textos, metric=tipo_distancia)
 
-set('hola este es un texto de prueba'.split())
+    def L1(self, textos):
+        if isinstance(textos, str) or len(textos) < 2:
+            print ('Debe ingresar una lista de por lo menos dos textos para hacer la comparación.')
+            return None
+        vectores = self.vectorizador.vectorizar(textos)
+        if len(vectores) == 2:
+            return pairwise_distances(vectores[None,0], vectores[None,1], dense_output=denso)
+        else:
+            return pairwise_distances(vectores, dense_output=denso)    
 
-
-import numpy as np
-a = np.array([1,2,1,4,1]).reshape(1,-1)
-b = np.array([1,1,1,3,1]).reshape(1,-1)
-
-c = np.vstack([a,b])
-inter = np.min(c, axis=0)
-tot = np.max(c, axis=0)
-inter/tot
-
-
-from sklearn.metrics import jaccard_similarity_score
-jaccard_similarity_score(a,b)
-
-
-
-
-a = np.array([0,1,1,1,4]).reshape(1,-1)
-b = np.array([0,0,1,0.1,2]).reshape(1,-1)
-
-
-
-
-
-
-
-c = cosine_similarity(a)
-
-np.expand_dims(a,0).shape
-a.shape
-
-a[None,0,:].shape
-
-c = cosine_similarity(a, b)
-c.shape
-
-Anorm = A / np.linalg.norm(A, axis=-1)[:, np.newaxis]
-return np.dot(Anorm, Anorm.T)
-
-cos_sim = np.inner(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
-vectores_prueba = v_hash.vectorizar(textos_prueba)
-vectores_prueba.shape
-len(vectores_prueba)
