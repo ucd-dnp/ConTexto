a2477dfba43f9484c35cfd3de0e6ba5385512d47
Pablo Montenegro Helfer <pabmontenegro@dnp.gov.co>
2020-09-08 09:21:58 -0500

    Se escribió la documentación de pre_ocr y ocr

--- contexto/utils/pre_ocr.py
+++ contexto/utils/pre_ocr.py
@@ -11,6 +11,11 @@ from PIL import Image
 
 
 def graficar_img(img, titulo='', dims=(15, 10)):
+    """ Función que grafica y muestra una imagen con Pyplot de Matplotlib.
+    :param img: (array). Imagen que se desea graficar
+    :param titulo: (string). Título de la gráfica
+    :param dims: (tuple). Tamaño (figsize) del gráfico de Pyplot
+    """
     plt.figure(figsize=dims)
     plt.title(titulo)
     if len(img.shape) == 2:
@@ -26,16 +31,34 @@ def graficar_img(img, titulo='', dims=(15, 10)):
 
 
 def blur_img(img):
+    """ Función que aplica blurring o 'borrosidad' a una imagen con el fin de \
+        eliminar ruido y facilitar la extracción de texto
+    :param img: (array). Imagen sobre la cual se aplica blurring
+    """
     return cv2.medianBlur(img, 5)
 
 # Aplicar 'thresholding' (umbral para decidir valores)
 
 
 def umbral_otsu(img):
+    """ Función que aplica el umbral de OTSU sobre una imagen, con el fin de \
+    facilitar la extracción del texto
+    :param img: (array). Imagen sobre la cual se aplica el umbral de OTSU
+    """
     return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
 
 def umbral_adaptivo(img, tipo='gaussian'):
+    """ Función que aplica el umbral adaptativo sobre una imagen, con el fin de \
+        facilitar la extracción del texto. Suele aplicarse cuando la iluminación es 
+        desigual a lo largo de la imagen
+    :param img: (array). Imagen sobre la cual se aplica el umbral adaptativo
+    :param tipo: (string) {'gaussian', None}. En caso de ser 'gaussian', el valor \
+        del umbral es igual a la suma ponderada gaussiana de los valores vecinos \
+        menos una constante. En caso de ser None, el valor del umbral es igual \
+        al promedio de los valores vecino menos una constante ()
+    """
+
     if tipo == 'gaussian':
         return cv2.adaptiveThreshold(
             img,
@@ -55,6 +78,9 @@ def umbral_adaptivo(img, tipo='gaussian'):
 
 
 def corregir_giro(img):
+    """ Función que corrige el alineamiento del texto dentro de una imagen
+    :param img: (array). Imagen sobre la cual se aplica la correcciónd e alineamiento
+    """
     # Adaptada de
     # https://www.pyimagesearch.com/2017/02/20/text-skew-correction-opencv-python/
     invertida = cv2.bitwise_not(img)
@@ -73,23 +99,42 @@ def corregir_giro(img):
 
 
 def procesar_img_1(img, enderezar=True):
+    """ Primera función de preprocesamiento. Se encarga de convertir una imagen a escala de grises
+    :param img: (array). Imagen sobre la cual aplica el preprocesamiento
+
+    """
     gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return gris
 
 
 def procesar_img_2(img, enderezar=True):
+    """ Segunda función de preprocesamiento. Se encarga de convertir una imagen a escala de grises \
+        y aplicar blurring ('borrosidad')
+    :param img: (array). Imagen sobre la cual aplica el preprocesamiento 
+    """
     gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     borrosa = blur_img(gris)
     return borrosa
 
 
 def procesar_img_3(img, enderezar=True):
+    """ Tercera función de preprocesamiento. Se encarga de convertir una imagen a escala de grises \
+        y aplicar el umbral de imagen con el método OTSU
+    :param img: (array). Imagen sobre la cual aplica el preprocesamiento 
+    """
     gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gris = umbral_otsu(gris)
     return gris
 
 
 def procesar_img_4(img, enderezar=True):
+    """ Cuarta función de preprocesamiento. Se encarga de convertir una imagen a escala de grises, \
+        aplicar blurring ('borrosidad') y umbral adaptativo. Adicionalmente, permite enderezar el 
+        texto de la imagen
+    :param img: (array). Imagen sobre la cual aplica el preprocesamiento 
+    :param enderezar: (bool) {True, False}. Valor por defecto: True. Permite enderezar el texto \
+        de la imagen para obtener mejores resultados durante el proceso de extracción de texto
+    """
     gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     borrosa = blur_img(gris)
     salida = umbral_adaptivo(borrosa)
@@ -99,6 +144,14 @@ def procesar_img_4(img, enderezar=True):
 
 
 def procesar_img_5(img, enderezar=True):
+    """ Quinta función de preprocesamiento. Se encarga de convertir una imagen a escala de grises, \
+    aplicar blurring ('borrosidad') y, umbral de OTSU y umbral adaptativo. Adicionalmente, permite enderezar el 
+    texto de la imagen
+    :param img: (array). Imagen sobre la cual aplica el preprocesamiento
+    :param enderezar: (bool) {True, False}. Valor por defecto: True. Permite enderezar el texto \
+        de la imagen para obtener mejores resultados durante el proceso de extracción de texto
+    """
+
     gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gris = umbral_otsu(gris)
     salida = blur_img(gris)
