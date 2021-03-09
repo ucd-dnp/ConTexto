9d0b5c94eb8f9916a484247ac684f512d3121be5
David Insuasti <davidinsuasty@gmail.com>
2021-03-09 18:39:06 -0500

    dispersion_plot.py en formato pep8

--- contexto/utils/dispersion_plot.py
+++ contexto/utils/dispersion_plot.py
@@ -7,21 +7,28 @@ import warnings
 
 # Objeto dispersionPlot
 class dispersionPlot:
-    def __init__(self, text, keywords, ignore_case=True,
-                 title='Gráfico de dispersión de palabras',
-                 label_x='Distribución de términos',
-                 label_y='Términos de interés',
-                 labels=None,
-                 auto_labels=True,
-                 figsize=(12, 7),
-                 marker='|', marker_size=20, marker_width=3,
-                 colors=None, cm='nipy_spectral',
-                 legend=True,
-                 rotation=30,
-                 show=True,
-                 outpath=None,
-                 return_fig=False
-                 ):
+    def __init__(
+        self,
+        text,
+        keywords,
+        ignore_case=True,
+        title="Gráfico de dispersión de palabras",
+        label_x="Distribución de términos",
+        label_y="Términos de interés",
+        labels=None,
+        auto_labels=True,
+        figsize=(12, 7),
+        marker="|",
+        marker_size=20,
+        marker_width=3,
+        colors=None,
+        cm="nipy_spectral",
+        legend=True,
+        rotation=30,
+        show=True,
+        outpath=None,
+        return_fig=False,
+    ):
         self._ignore_case = ignore_case
         self.textos = text
         self.keywords = keywords
@@ -55,10 +62,14 @@ class dispersionPlot:
             all_words = self._textos.split()
         elif isinstance(text, list):
             self._textos = text
-            all_words = ' '.join(self._textos).split()
+            all_words = " ".join(self._textos).split()
         else:
             raise ValueError(
-                'Tipo de datos desconocido, por favor ingrese un texto o una lista de textos')
+                (
+                    "Tipo de datos desconocido, por favor ingrese un texto "
+                    "o una lista de textos"
+                )
+            )
 
         if self._ignore_case:
             self._all_words = list(map(str.lower, all_words))
@@ -77,15 +88,15 @@ class dispersionPlot:
 
             if len(np.unique(keywords)) != len(keywords):
                 warnings.warn(
-                    'Existen palabras clave repetidas. Estás serán eliminadas')
+                    "Existen palabras clave repetidas. Estás serán eliminadas"
+                )
                 indexes = np.unique(keywords, return_index=True)[1]
                 keywords = [keywords[index] for index in sorted(indexes)]
                 keywords.reverse()
             self._comprobar_existencia(keywords)
 
         else:
-            raise ValueError(
-                'Por favor ingrese una lista términos o palabras clave')
+            raise ValueError("Por favor ingrese una lista términos de interés")
 
     @property
     def labels(self):
@@ -94,7 +105,7 @@ class dispersionPlot:
     @labels.setter
     def labels(self, labels):
         if labels is None and self._autolabels:
-            self._labels = [f'Doc {i+1}' for i in range(len(self.textos))]
+            self._labels = [f"Doc {i+1}" for i in range(len(self.textos))]
         elif labels is None:
             self._labels = None
         elif isinstance(labels, list):
@@ -102,10 +113,18 @@ class dispersionPlot:
                 self._labels = labels
             else:
                 raise ValueError(
-                    'El número de etiquetas de entrada no es igual al número de documentos.')
+                    (
+                        "El número de etiquetas de entrada no es igual al "
+                        "número de documentos."
+                    )
+                )
         else:
             raise ValueError(
-                'El tipo de datos de las etiquetas no está permitido, por favor ingrese una lista de etiquetas.')
+                (
+                    "El tipo de datos de las etiquetas no está permitido, "
+                    "por favor ingrese una lista de etiquetas."
+                )
+            )
 
     @property
     def colors(self):
@@ -120,20 +139,31 @@ class dispersionPlot:
                 self._colors = colors
             else:
                 raise ValueError(
-                    'El número de colores de entrada debe ser igual al número de documentos')
+                    (
+                        "El número de colores de entrada debe ser igual al "
+                        "número de documentos"
+                    )
+                )
         else:
             raise ValueError(
-                'Tipo de datos en colores es desconocido, por favor ingrese una lista de colores.')
+                (
+                    "Tipo de datos en colores es desconocido, por favor"
+                    "ingrese una lista de colores."
+                )
+            )
 
     def _set_limites_x(self):
         """
-        Función para calcular finales de documentos y posición de etiquetas en plot
+        Función para calcular finales de documentos y posición de \
+        etiquetas en gráfico de dispersión.
         """
         limits = [len(t.split()) for t in self._textos]
         limits = np.cumsum(limits) - 1
         limits_ = np.insert(limits, 0, 0)
-        x_pos = [limits_[i] + (limits_[i + 1] - limits_[i]) /
-                 2 for i in range(len(limits))]
+        x_pos = [
+            limits_[i] + (limits_[i + 1] - limits_[i]) / 2
+            for i in range(len(limits))
+        ]
         self._limits = limits
         self._x_limits = x_pos
 
@@ -145,10 +175,15 @@ class dispersionPlot:
         if no_words:
             if len(no_words) == len(keywords):
                 raise ValueError(
-                    'No existe ninguna palabra clave asociada a los documentos.')
+                    ("No existe palabras claves asociada a los documentos.")
+                )
+            no_words = ", ".join(no_words)
             warnings.warn(
-                'Advertencia: las palabras: ({}) no están en los documentos de entrada'.format(
-                    ', '.join(no_words)))
+                (
+                    "Advertencia: las palabras: ({})".format(no_words)
+                    + " no están en los documentos de entrada"
+                )
+            )
             self._keywords = [w for w in keywords if w not in no_words]
         else:
             self._keywords = keywords
@@ -159,8 +194,10 @@ class dispersionPlot:
         """
         cmap = cm.get_cmap(self._cm)
         niveles = len(self._textos)
-        colores = [cmap(c / (2 * niveles)) if c %
-                   2 else cmap(0.5 + c / (2 * niveles)) for c in range(niveles)]
+        colores = [
+            cmap(c / (2 * niveles)) if c % 2 else cmap(0.5 + c / (2 * niveles))
+            for c in range(niveles)
+        ]
         return colores
 
     def _calcular_dispersion(self):
@@ -195,7 +232,8 @@ class dispersionPlot:
                     self._marker,
                     ms=self._marker_size,
                     mew=self._marker_width,
-                    color=self.colors[i])
+                    color=self.colors[i],
+                )
 
             else:
                 d_ant = self._limits[i - 1]
@@ -205,34 +243,44 @@ class dispersionPlot:
                     self._marker,
                     ms=self._marker_size,
                     mew=self._marker_width,
-                    color=self._colors[i])
+                    color=self._colors[i],
+                )
 
-            ax.axvline(x=d + 0.5, color='lightgray',
-                       linestyle='dashed')
+            ax.axvline(x=d + 0.5, color="lightgray", linestyle="dashed")
 
         box = ax.get_position()
         ax.set_position([box.x0, box.y0, box.width * 0.85, box.height])
-        ax.set_title(self._title, {'fontsize': 15, 'fontweight': 700})
+        ax.set_title(self._title, {"fontsize": 15, "fontweight": 700})
         ax.set_xlabel(self._label_x)
         ax.set_xlim(0.2, x[-1] + 0.2)
         ax.set_xticks(self._x_limits)
         if self._labels is not None:
             ax.set_xticklabels(self._labels, rotation=self._rotation)
             if self._legend:
-                ax.legend(lines, self._labels, bbox_to_anchor=(1.003, 1),
-                          loc='upper left', markerscale=0.5, frameon=False)
+                ax.legend(
+                    lines,
+                    self._labels,
+                    bbox_to_anchor=(1.003, 1),
+                    loc="upper left",
+                    markerscale=0.5,
+                    frameon=False,
+                )
         ax.set_ylabel(self._label_y)
         ax.set_yticks(list(range(len(self.keywords))))
         ax.set_yticklabels(self._keywords)
 
         if self._outpath is not None:
-            plt.savefig(self._outpath, bbox_inches='tight',
-                        transparent=False, facecolor='w', dpi=300)
+            plt.savefig(
+                self._outpath,
+                bbox_inches="tight",
+                transparent=False,
+                facecolor="w",
+                dpi=300,
+            )
         if self._show:
             plt.show()
         if not self._show and self._outpath is None:
-            warnings.warn(
-                'por favor fije un directorio para guardar la imagen')
+            warnings.warn("Por favor fije una ruta para guardar la imagen")
             plt.show()
         if self.return_fig:
             return fig
