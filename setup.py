8f13bb6388165faf77592a05fbaed69968a3ccf2
David Insuasti <davidinsuasty@gmail.com>
2021-06-30 15:47:18 -0500

    cambios de prueba para lanzamiento

--- setup.py
+++ setup.py
@@ -1,49 +1,51 @@
 import setuptools
 import sys
 
-with open("README.md", "r", encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-INSTALL_REQUIRES = ['docx2txt==0.8',
-                    'gensim==3.8.3',
-                    'googletrans==2.4.0',
-                    'jellyfish==0.8.2',
-                    'langid==1.1.6',
-                    'matplotlib==3.3.1',
-                    'networkx==2.5',
-                    'nltk==3.5',
-                    'numpy==1.19.1',
-                    'pandas==1.1.1',
-                    'pdf2image==1.14.0',
-                    'pyspellchecker==0.5.5',
-                    'PyPDF2==1.26.0',
-                    'python-docx==0.8.10',
-                    'pywin32==228; platform_system=="Windows"',
-                    'opencv-python==4.4.0.42',
-                    'pytesseract==0.3.5',
-                    'reportlab==3.5.48',
-                    'scikit-learn==0.23.2',
-                    'slate3k==0.5.3',
-                    'spacy==2.3.2',
-                    'wordcloud==1.8.0']
+INSTALL_REQUIRES = [
+    "docx2pdf",
+    "docx2txt==0.8",
+    "gensim==4.0.1",
+    "googletrans==2.4.0",
+    "jellyfish==0.8.2",
+    "langid==1.1.6",
+    "matplotlib>=3.3.4",
+    "networkx==2.5.1",
+    "nltk==3.6.2",
+    "pandas>=0.25.3",
+    "pdf2image==1.16.0",
+    "PyPDF2==1.26.0",
+    "pyspellchecker==0.6.2",
+    "pytesseract>=0.3.7",
+    "python-docx>=0.8.11",
+    'pywin32>=227; platform_system=="Windows"',
+    "opencv-python>=4.5.2.54",
+    "reportlab==3.5.68",
+    "scikit-learn>=0.24.2",
+    "slate3k==0.5.3",
+    "spacy>=3.0.6",
+    "wordcloud>=1.8.1",
+]
 
-PACKAGE_NAME = 'ConTexto'
+PACKAGE_NAME = "ConTexto"
 
 setuptools.setup(
     name=PACKAGE_NAME,
-    version='0.1.8',
+    version="0.2.0",
     author="Departamento Nacional de Planeación - DNP",
-    author_email='ucd@dnp.gov.co',
-    maintainer='Unidad de Científicos de Datos - UCD',
-    maintainer_email='ucd@dnp.gov.co',
+    author_email="ucd@dnp.gov.co",
+    maintainer="Unidad de Científicos de Datos - UCD",
+    maintainer_email="ucd@dnp.gov.co",
     description=(
         "Librería para el procesamiento y análisis de texto con Python"
     ),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license='MIT',
-    keywords='python OCR texto',
-    url='https://github.com/ucd-dnp/ConTexto',
+    license="MIT",
+    keywords="python OCR texto",
+    url="https://github.com/ucd-dnp/ConTexto",
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=INSTALL_REQUIRES,
@@ -55,6 +57,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    python_requires=">=3.6.*"
+    python_requires=">=3.6.0",
 )
