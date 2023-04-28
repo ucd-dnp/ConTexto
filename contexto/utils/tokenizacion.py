5e6c34603501804a2f6e331087d9e67b82a756f1
DevArKaDiA <jfernandorojasc@gmail.com>
2023-04-28 00:33:46 -0500

    move iterable from collections to abc

--- contexto/utils/tokenizacion.py
+++ contexto/utils/tokenizacion.py
@@ -1,4 +1,4 @@
-from collections import Iterable
+from collections.abc import Iterable
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 from nltk.tokenize.toktok import ToktokTokenizer
 
